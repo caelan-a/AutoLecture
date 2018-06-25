6eb66041ed9a1bfcddfa43a08c08bd8f044e779c
Caelan <caelan.andsn@gmail.com>
2018-06-25 15:17:35 +1000

    Implemented framework for multithreading

--- front/frames/LoadingFrame.py
+++ front/frames/LoadingFrame.py
@@ -1,14 +1,35 @@
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+from workthreads.GetSubjectInfoThread import GetSubjectInfoThread
+
 class LoadingFrame(QWidget):
+	@Slot(str)
+	def setLoadingText(self, info_text):
+		self.label_loading_info.setText(info_text)
+
+	def startGetSubjectInfoThread(self):
+		if not self.get_subject_info_thread.isRunning():
+			self.get_subject_info_thread.exiting=False
+			self.get_subject_info_thread.start()
+
+	def show(self):
+		super().show()
+		# Setup thread to get subject info
+		self.get_subject_info_thread = GetSubjectInfoThread(self, self.setLoadingText)
+		self.startGetSubjectInfoThread()
+		self.label_loading_anim.movie().start()
+
 	def __init__(self, loading_anim_movie):
 		QWidget.__init__(self)
 
-		self.layout = QVBoxLayout()
+		# Setup thread to get subject info
+		self.get_subject_info_thread = GetSubjectInfoThread(self, self.setLoadingText)
 
+		self.layout = QVBoxLayout()
 		self.layout.setAlignment(Qt.AlignHCenter)
 
+		# Setup animation
 		self.label_loading_anim = QLabel()
 		self.label_loading_anim.setStyleSheet('background:transparent;')
 		loading_anim_movie.setSpeed(400)
@@ -19,8 +40,14 @@ class LoadingFrame(QWidget):
 		self.label_loading_anim.setGraphicsEffect(self.effect)
 		self.effect.setColor(QColor('#2F9599'))
 
-		loading_anim_movie.start()
+		# Info string about current action
+		self.label_loading_info = QLabel("Loading info")
+		self.label_loading_info.setAlignment(Qt.AlignCenter)
 
 		self.layout.addWidget(self.label_loading_anim)
 		self.layout.addStretch()
+		self.layout.addWidget(self.label_loading_info)
+		self.layout.addStretch()
 		self.setLayout(self.layout)
+
+
